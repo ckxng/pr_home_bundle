59a8303356ae6b5a4772bbcbb2158aaae218e2c9
Cameron King <cking@ecc12.com>
2013-04-07 21:58:10 -0500

    split hello and index

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -9,10 +9,20 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/hello/", defaults={"name" = "Friend"})
      * @Route("/hello/{name}")
      * @Template()
      */
-    public function indexAction($name)
+    public function helloAction($name)
     {
         return array('name' => $name);
     }
