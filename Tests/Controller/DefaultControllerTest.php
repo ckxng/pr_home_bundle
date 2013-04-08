14d033ac1e2795eea58920920730192953388ba9
Cameron King <cking@ecc12.com>
2013-04-07 22:07:39 -0500

    add test case for index

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -10,6 +10,15 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($crawler->filter('html:contains("Back to top")')->count() > 0);
+    }
+
+    public function testHello()
+    {
+        $client = static::createClient();
+
         $crawler = $client->request('GET', '/hello/Fabien');
 
         $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
