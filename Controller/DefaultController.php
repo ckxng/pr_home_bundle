75f6f315e97447e1ffa5e84e88d9b3406103157c
Cameron King <cking@ecc12.com>
2013-04-10 21:25:51 -0500

    Add directory controller, template, and sample directory files

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="index")
      * @Template()
      */
     public function indexAction()
@@ -18,12 +18,21 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/hello/", defaults={"name" = "Friend"})
-     * @Route("/hello/{name}")
+     * @Route("/hello/{in_name}", name = "hello", defaults={"in_name" = "Friend"})
      * @Template()
      */
-    public function helloAction($name)
+    public function helloAction($in_name)
     {
-        return array('name' => $name);
+        return array('name' => $in_name);
+    }
+
+    /**
+     * @Route("/directory/{family_id}", name="directory", defaults={"family_id" = -1})
+     * @Template()
+     */
+    public function directoryAction($family_id)
+    {
+        return array("file_path" => $this->get('kernel')->
+            locateResource("@Ecc12PRHomeBundle/Resources/private/directory.xml"));
     }
 }
