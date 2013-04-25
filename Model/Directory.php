b87924b2aef6955da1999dfdff30905112db4cf1
Cameron King <cking@ecc12.com>
2013-04-25 20:09:20 +0000

    add cdn to base template

---
+++ Model/Directory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ckxng
+ * Date: 4/20/13
+ * Time: 9:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Ecc12\PRHomeBundle\Model;
+
+
+class Directory {
+
+}
